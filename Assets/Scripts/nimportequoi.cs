7d20ca65e7630627cbfa75312b1a866a646bbdec
EricPiaugeard <98194635+EricPiaugeard@users.noreply.github.com>
2022-01-21 19:38:55 -0500

    Modification de script
    Crée un script inutile et fixer l'erreur de fake script

--- Assets/Scripts/nimportequoi.cs
+++ Assets/Scripts/nimportequoi.cs
@@ -2,12 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FakeScript : MonoBehaviour
+public class nimportequoi : MonoBehaviour
 {
-    int  life=0;
     // Start is called before the first frame update
     void Start()
     {
+        
     }
 
     // Update is called once per frame
@@ -15,5 +15,4 @@ public class FakeScript : MonoBehaviour
     {
         
     }
-    }
-
+}
