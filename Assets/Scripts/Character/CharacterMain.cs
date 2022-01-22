30db59143fd79e4da3ba0b3d2e34f5c0e86f1678
louishorlaville <louis.horlaville@gmail.com>
2022-01-22 15:51:26 -0500

    basic left right movements on character

--- Assets/Scripts/Character/CharacterMain.cs
+++ Assets/Scripts/Character/CharacterMain.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class CharacterMain : MonoBehaviour
 {
+    Rigidbody2D rb;
+
+    public string currentShape = "circle";
+    public float movementSpeed;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = gameObject.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void FixedUpdate()
+    {
+        float movementH = Input.GetAxis("Horizontal");
+        rb.AddForce(new Vector2(movementH * movementSpeed, 0f), ForceMode2D.Force);
     }
 }
