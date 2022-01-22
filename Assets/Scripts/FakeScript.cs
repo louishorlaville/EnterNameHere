5475d43b4fd61ab9ecbb1632968cb9605c4c5bde
louishorlaville <louis.horlaville@gmail.com>
2022-01-21 19:19:45 -0500

    fake hack
    deeznuts

--- Assets/Scripts/FakeScript.cs
+++ Assets/Scripts/FakeScript.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class FakeScript : MonoBehaviour
 {
+    int  life=0;
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
     }
 }
