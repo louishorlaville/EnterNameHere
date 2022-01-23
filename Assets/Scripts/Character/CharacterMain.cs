1f30ea6cad0be9eb8b8327a1f85aa1af2ca58aa5
louishorlaville <louis.horlaville@gmail.com>
2022-01-22 23:16:43 -0500

    Bounce + in air rotate

--- Assets/Scripts/Character/CharacterMain.cs
+++ Assets/Scripts/Character/CharacterMain.cs
@@ -12,6 +12,7 @@ public class CharacterMain : MonoBehaviour
 
     bool toSquare;
     bool toCircle;
+    float zAxis;
 
     public Sprite[] sprites;
     public bool isCircle = true;
@@ -52,8 +53,32 @@ public class CharacterMain : MonoBehaviour
 
     void FixedUpdate()
     {
-        float movementH = Input.GetAxis("Horizontal");
-        rb.AddForce(new Vector2(movementH * movementSpeed, 0f), ForceMode2D.Impulse);
+
+        float _movementH = Input.GetAxis("Horizontal");
+        if(_movementH >= 0.1 || _movementH <= 0.1)
+        {
+            if (isGrounded())
+            {
+                rb.AddForce(new Vector2(_movementH * movementSpeed, 0f), ForceMode2D.Impulse);
+            }
+            else
+            {
+                zAxis += (Time.deltaTime * _movementH * 150);
+                transform.rotation = Quaternion.Euler(0, 0, -zAxis);
+            }
+        }
+
+    }
+
+    //Check if player is touching a surface
+    public bool isGrounded()
+    {
+        bool _isGrounded;
+
+        RaycastHit2D circleCast = Physics2D.CircleCast(transform.position, 4, new Vector2(0, 0));
+        _isGrounded = circleCast.collider != null;
+
+        return _isGrounded;
     }
 
     private void SwitchToSquare()
@@ -72,55 +97,17 @@ public class CharacterMain : MonoBehaviour
         renderer.sprite = sprites[0];  
     }
 
+    //Casts a circleCast to detect which side is on the ground and launch player in surface normal direction
     public void BounceAlongNormal()
     {
-        RaycastHit2D hitUp = Physics2D.Raycast(transform.position, transform.TransformDirection(Vector3.up), 2.8f);
-        RaycastHit2D hitDown = Physics2D.Raycast(transform.position, transform.TransformDirection(Vector3.down), 2.8f);
-        RaycastHit2D hitLeft = Physics2D.Raycast(transform.position, transform.TransformDirection(Vector3.left), 2.8f);
-        RaycastHit2D hitRight = Physics2D.Raycast(transform.position, transform.TransformDirection(Vector3.right), 2.8f);
-        Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.up) * 2.8f, Color.red);
-
-        if (hitUp.collider!=null)
+        RaycastHit2D circleCast = Physics2D.CircleCast(transform.position, 3f, new Vector2(0,0));
+        if (circleCast.collider != null)
         {
-            Debug.Log("Up Hit: "+ hitUp.collider.gameObject.name);
-            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.up)*5, Color.red);
-            
-            Debug.DrawRay(new Vector3(hitUp.normal.x, hitUp.normal.y, 0), hitUp.collider.transform.TransformDirection(hitUp.normal)*20, Color.red);
+            Debug.DrawRay(new Vector3(circleCast.normal.x, circleCast.normal.y, 0), circleCast.collider.transform.TransformDirection(circleCast.normal) * 20, Color.red);
 
             //convert normal direction to force
-            hitUp.normal.Normalize();
-            rb.AddForce(hitUp.normal*bounceHeight, ForceMode2D.Impulse);
-        }
-        if (hitDown.collider != null)
-        {
-            Debug.Log("Up Hit: " + hitDown.collider.gameObject.name);
-            Debug.DrawRay(new Vector3(hitDown.normal.x, hitDown.normal.y, 0), hitDown.collider.transform.TransformDirection(hitDown.normal) * 20, Color.red);
-
-            //convert normal direction to force
-            hitDown.normal.Normalize();
-            rb.AddForce(hitDown.normal * bounceHeight, ForceMode2D.Impulse);
-        }
-        if (hitLeft.collider != null)
-        {
-            Debug.Log("Up Hit: " + hitLeft.collider.gameObject.name);
-            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.left) * 5, Color.red);
-
-            Debug.DrawRay(new Vector3(hitLeft.normal.x, hitLeft.normal.y, 0), hitLeft.collider.transform.TransformDirection(hitLeft.normal) * 20, Color.red);
-
-            //convert normal direction to force
-            hitLeft.normal.Normalize();
-            rb.AddForce(hitLeft.normal * bounceHeight, ForceMode2D.Impulse);
-        }
-        if (hitRight.collider != null)
-        {
-            Debug.Log("Up Hit: " + hitRight.collider.gameObject.name);
-            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.right) * 5, Color.red);
-
-            Debug.DrawRay(new Vector3(hitRight.normal.x, hitRight.normal.y, 0), hitRight.collider.transform.TransformDirection(hitRight.normal) * 20, Color.red);
-
-            //convert normal direction to force
-            hitRight.normal.Normalize();
-            rb.AddForce(hitRight.normal * bounceHeight, ForceMode2D.Impulse);
+            circleCast.normal.Normalize();
+            rb.AddForce(circleCast.normal * bounceHeight, ForceMode2D.Impulse);
         }
 
     }
