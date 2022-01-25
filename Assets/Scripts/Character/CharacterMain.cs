61871704b56c3684d2f94b6c0cd7bd6db6ca9542
louishorlaville <louis.horlaville@gmail.com>
2022-01-24 23:35:10 -0500

    Bounce on surface is working correctly

--- Assets/Scripts/Character/CharacterMain.cs
+++ Assets/Scripts/Character/CharacterMain.cs
@@ -10,8 +10,12 @@ public class CharacterMain : MonoBehaviour
     SpriteRenderer renderer;
 
     bool toSquare;
+    bool canBounce=true;
     bool toCircle;
     float zAxis;
+    float bounciness;
+    int maxNbBounces = 3;
+    int currentBounces = 0;
 
     public Sprite[] sprites;
     public bool isCircle = true;
@@ -19,6 +23,7 @@ public class CharacterMain : MonoBehaviour
     public float bounceHeight;
     public float rollSpeed;
     public float rollRotationSpeed;
+    public float collisionBounceHeight;
 
     // Start is called before the first frame update
     void Start()
@@ -27,7 +32,6 @@ public class CharacterMain : MonoBehaviour
         circleCollider = gameObject.GetComponent<CircleCollider2D>();
         boxCollider = gameObject.GetComponent<BoxCollider2D>();
         renderer = gameObject.GetComponent<SpriteRenderer>();
-
     }
 
     // Update is called once per frame
@@ -40,19 +44,20 @@ public class CharacterMain : MonoBehaviour
         if (Input.GetKey("space"))
         {
             SwitchToSquare();
+            
         }
 
         if (Input.GetKeyUp("space"))
         {
-            BounceAlongNormal();
+            //BounceAlongNormal();
             SwitchToCircle();
+            
         }
     }
 
     void FixedUpdate()
     {
         Debug.DrawLine(transform.position, transform.position + (Vector3.down + Vector3.right) * 2.2f, Color.blue);
-        Debug.Log(rb.velocity.x);
 
         float _movementH = Input.GetAxis("Horizontal");
 
@@ -86,15 +91,31 @@ public class CharacterMain : MonoBehaviour
             rb.AddForce(new Vector2(0f, 0f), ForceMode2D.Impulse);
         }
     }
-    
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {   
+        if (currentBounces<maxNbBounces && isCircle && canBounce)
+        {
+            currentBounces++;
+            bounciness = collisionBounceHeight / currentBounces;
+            collision.contacts[0].normal.Normalize();
+            rb.velocity = collision.contacts[0].normal* (collisionBounceHeight / Mathf.Pow(2, currentBounces-1));
+        }
+        else
+        {
+            bounciness = collisionBounceHeight;
+            currentBounces = 0;
+            canBounce = false;
+        }
+    }
 
     //Check if player is touching a surface
     public bool isGrounded()
     {
         bool _isGrounded;
 
-        RaycastHit2D circleCast = Physics2D.CircleCast(transform.position, 4, new Vector2(0, 0));
-        _isGrounded = circleCast.collider != null;
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector3.down, 4);
+        _isGrounded = hit.collider != null;
 
         return _isGrounded;
     }
@@ -105,6 +126,7 @@ public class CharacterMain : MonoBehaviour
         circleCollider.enabled = false;
         isCircle = false;
         renderer.sprite = sprites[1];
+        canBounce = false;
     }
 
     private void SwitchToCircle()
@@ -112,11 +134,13 @@ public class CharacterMain : MonoBehaviour
         boxCollider.enabled = false;
         circleCollider.enabled = true;
         isCircle = true;
-        renderer.sprite = sprites[0];  
+        renderer.sprite = sprites[0];
+        currentBounces = 0;
+        canBounce = true;
     }
 
     //Casts a circleCast to detect which side is on the ground and launch player in surface normal direction
-    public void BounceAlongNormal()
+    /*public void BounceAlongNormal()
     {
         RaycastHit2D circleCast = Physics2D.CircleCast(transform.position, 3f, new Vector2(0,0));
         if (circleCast.collider != null)
@@ -127,5 +151,5 @@ public class CharacterMain : MonoBehaviour
             circleCast.normal.Normalize();
             rb.AddForce(circleCast.normal * bounceHeight, ForceMode2D.Impulse);
         }
-    }
+    }*/
 }
