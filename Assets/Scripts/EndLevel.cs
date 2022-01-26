65ac3bc769110f52075de227a484f2832f67a67e
BeauAnto <antoine.beauchesne@outlook.com>
2022-01-26 15:55:20 -0500

    Finalisation du EndLevel prefab
     - Il y a des animations
     - Le Player ne peut pu rester prit

--- Assets/Scripts/EndLevel.cs
+++ Assets/Scripts/EndLevel.cs
@@ -5,44 +5,36 @@ using UnityEngine;
 public class EndLevel : MonoBehaviour
 {
     public GameObject holeCollider;
+    public GameObject spinnerL;
+    public GameObject spinnerR;
 
-    private IEnumerator coroutine;
-
-    // Start is called before the first frame update
-    private void Start()
-    {
-        
-    }
-
-	private void OnTriggerEnter2D(Collider2D _target)
+    private void OnTriggerEnter2D(Collider2D _target)
 	{
         if(_target.tag == "Player")
         {
-            Debug.Log("End");
             gameObject.GetComponent<Collider2D>().enabled = false;
 
-            coroutine = EndLevelCoroutine(_target);
-            StartCoroutine(coroutine);
+            StartCoroutine(EndLevelCoroutine(_target));
         }
     }
 
     IEnumerator EndLevelCoroutine(Collider2D _target)
 	{
-        Debug.Log("Coroutine");
         float _elapsedTime = 0;
-        float _waitTime = 2;
+        float _waitTime = 3;
 
         _target.GetComponent<CharacterMain>().bEndLevel = true;
         _target.GetComponent<Rigidbody2D>().velocity = new Vector2(0f, 0f);
         _target.GetComponent<Rigidbody2D>().gravityScale = 0f;
 
-        //yield return new WaitForSeconds(0.5f);
-
         while(_elapsedTime < _waitTime)
 		{
-            _target.transform.position = Vector2.Lerp(_target.transform.position, new Vector3(gameObject.transform.position.x, gameObject.transform.position.y + 1, gameObject.transform.position.z), (_elapsedTime / _waitTime));
+            _target.transform.position = Vector2.Lerp(_target.transform.position, new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z), (_elapsedTime / _waitTime));
             _target.transform.rotation = Quaternion.Lerp(_target.transform.rotation, new Quaternion(0f, 0f, 0f, 0f), (_elapsedTime * 0.25f));
-            
+
+            spinnerL.transform.rotation = Quaternion.Lerp(spinnerL.transform.rotation, _target.transform.rotation, (_elapsedTime * 0.25f));
+            spinnerR.transform.rotation = Quaternion.Lerp(spinnerR.transform.rotation, _target.transform.rotation, (_elapsedTime * 0.25f));
+
             _elapsedTime += Time.deltaTime;
             
             yield return null;
@@ -54,6 +46,6 @@ public class EndLevel : MonoBehaviour
 
         _target.GetComponent<Rigidbody2D>().gravityScale = 10f;
 
-        //StopCoroutine(coroutine);
+        yield return null;
     }
 }
