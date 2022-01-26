fa37509414cacf653d438d10b1135b56af8cab89
Thepeel89 <thepeel89@gmail.com>
2022-01-26 14:37:36 -0500

    Conflict fix

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 /////////////////////////////////////////////////////////////////////////////////////////////////////
 //
 // Audiokinetic Wwise generated include file. Do not edit.
@@ -51,3 +52,83 @@ namespace AK
 }// namespace AK
 
 #endif // __WWISE_IDS_H__
+=======
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID PLAYER_JUMP = 1305133589U;
+    } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace IN_GAME_MENU
+        {
+            static const AkUniqueID GROUP = 4261341665U;
+
+            namespace STATE
+            {
+                static const AkUniqueID IN_MENU = 1631528850U;
+                static const AkUniqueID NONE = 748895195U;
+                static const AkUniqueID OUT_OF_MENU = 860659345U;
+            } // namespace STATE
+        } // namespace IN_GAME_MENU
+
+        namespace MAGNET_POLARITY
+        {
+            static const AkUniqueID GROUP = 4033083518U;
+
+            namespace STATE
+            {
+                static const AkUniqueID NEGATIVE = 4219547688U;
+                static const AkUniqueID NONE = 748895195U;
+                static const AkUniqueID POSITIVE = 1192865152U;
+            } // namespace STATE
+        } // namespace MAGNET_POLARITY
+
+        namespace PLAYER_FORM
+        {
+            static const AkUniqueID GROUP = 2582558235U;
+
+            namespace STATE
+            {
+                static const AkUniqueID CIRCLE = 240409309U;
+                static const AkUniqueID CUBE = 4052031814U;
+                static const AkUniqueID NONE = 748895195U;
+            } // namespace STATE
+        } // namespace PLAYER_FORM
+
+    } // namespace STATES
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID BANQUE_ERIC = 3529285007U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
+>>>>>>> Stashed changes
