89b9ecc4d5e569b62e96371c32de71e65d7111ca
BeauAnto <antoine.beauchesne@outlook.com>
2022-01-28 22:24:17 -0500

    Nouveau Level 2
     - Le Level 2 est maintenant nommé Level 7
     - Le Level 2 est un nouveau niveau

--- Assets/Scripts/ButtonInteraction.cs
+++ Assets/Scripts/ButtonInteraction.cs
@@ -5,13 +5,19 @@ using UnityEngine;
 [System.Serializable]
 public class ButtonInteraction : MonoBehaviour
 {
-    public bool moveAway;
+    public bool moveAway = false;
+    public bool setActive = false;
 
     public virtual void ButtonPressed()
     {
 		if(moveAway == true)
 		{
             gameObject.transform.position = new Vector3(1000f, 1000f, 0f);
+		}
+
+		if(setActive == true)
+		{
+			gameObject.SetActive(true);
 		}
     }
 }
