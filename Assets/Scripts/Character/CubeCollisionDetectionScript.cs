d1cc6821ff9c8f85f4b4f0f1ee33fb699caf68c5
louishorlaville <louis.horlaville@gmail.com>
2022-01-29 18:51:59 -0500

    cube movement sound detection
    detection working fine, just have to create wise event to post the actual sounds

---
+++ Assets/Scripts/Character/CubeCollisionDetectionScript.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeCollisionDetectionScript : MonoBehaviour
+{
+    GameObject player;
+    bool canCheck=true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = transform.parent.gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (!player.GetComponent<CharacterMain>().isCircle && canCheck)
+        {
+            player.GetComponent<CharacterMain>().EmitSoundCubeMovement();
+            canCheck = false; 
+            StartCoroutine(DelayBeforeCollisionCheck(2f));
+        }
+    }
+
+    IEnumerator DelayBeforeCollisionCheck(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+        canCheck = true;
+    }
+}
