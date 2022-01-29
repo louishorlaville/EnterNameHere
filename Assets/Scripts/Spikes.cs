69f1b6413b61f193c4e1fc1ad17b2f54e4f267bb
BeauAnto <antoine.beauchesne@outlook.com>
2022-01-28 23:48:18 -0500

    Correction du Level 6
     - Modification du script des spikes
     - Masquage du ref trigger sur le EndLevel
     - Tweaking du Level 6

--- Assets/Scripts/Spikes.cs
+++ Assets/Scripts/Spikes.cs
@@ -5,13 +5,11 @@ using UnityEngine.SceneManagement;
 
 public class Spikes : MonoBehaviour
 {
-	public string levelName;
-
 	private void OnCollisionEnter2D(Collision2D _target)
 	{
 		if(_target.transform.tag == "Player" && _target.gameObject.GetComponent<CharacterMain>().isCircle == true)
 		{
-			SceneManager.LoadScene(levelName);
+			SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
 		}
 	}
 }
