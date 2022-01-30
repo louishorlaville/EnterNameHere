5281f1e86e9b07316524b545ecb9df2a50fd18e5
louishorlaville <louis.horlaville@gmail.com>
2022-01-30 12:11:15 -0500

    Intro + removed colliders mush + removed transition text

--- Assets/Scripts/AudioIntroScript.cs
+++ Assets/Scripts/AudioIntroScript.cs
@@ -1,11 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.SceneManagement;
+
 
 public class AudioIntroScript : MonoBehaviour
 {
     public AK.Wwise.Event IntroVideo;
+    public VideoPlayer video;
 
+    bool canCheck = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +20,17 @@ public class AudioIntroScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        StartCoroutine("CheckIfDone");
+        if (canCheck && !video.isPlaying)
+        {
+            print(video.isPlaying);
+            SceneManager.LoadScene("Level-1");
+        }
+    }
+
+    public IEnumerator CheckIfDone()
+    {
+        yield return new WaitForSeconds(5f);
+        canCheck = true;
     }
 }
